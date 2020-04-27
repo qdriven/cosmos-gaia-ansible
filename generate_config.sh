4bbd22c194b9f22b468f319f39f6c51af10d5d3f
patrick <patrickwuke@163.com>
2020-04-27 16:47:20 +0800

    add modify ip address

--- generate_config.sh
+++ generate_config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 spawn echo "start generate cosmos config"
-spawn ./gaiad testnet --chain-id=cc-cosmos --starting-ip-address=127.168.3.94
+spawn ./gaiad testnet --chain-id=cc-cosmos --starting-ip-address=172.168.3.94
 expect "Password for account 'node0' (default 12345678):"
 send "12345678\r"
 expect "Password for account 'node1' (default 12345678):"
